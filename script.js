5b1e955b812016ec711ff81b2d413600427c4356
Kamari <kmoore971@gmail.com>
2023-07-21 09:50:40 -0500

    Add basic screen functionality

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 console.log("Keep Going 🍊");
+// DOM Maninpulation
 let btn = document.querySelectorAll(".btn");
 let currentScreen = document.querySelector(".screen-current");
+let lastScreen = document.querySelector(".screen-last");
+let clearButton = document.querySelector(".clear");
+let currentInput = '0';
+let lastInput = '0';
+let operator = '';
 
+// Screen functionality 
+const displayCurrentInput = () => {
+    currentScreen.textContent = currentInput;
+};
+displayCurrentInput();
+
+const displayLastInput = () => {
+    lastScreen.textContent = lastInput;
+};
+displayLastInput();
+
+const clearInput = () => {
+    currentInput = '0';
+    lastInput = '0';
+    operator = '';
+    // displayLastInput();
+    displayCurrentInput();
+};
+
+// button functionality
 let value = btn.forEach(btn => {
     btn.addEventListener("click", () => {
         // console.log(btn.textContent);
@@ -10,3 +36,4 @@ let value = btn.forEach(btn => {
     })
 });
 
+clearButton.addEventListener('click', clearInput);
